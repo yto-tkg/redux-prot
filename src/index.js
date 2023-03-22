ec172430302126f49b4b9ef5fa4fc9fba58ef534
yto-tkg <yuto-takagi@cybozu.co.jp>
2023-03-23 08:42:50 +0900

    fix: use reducer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux"
+
+// actions -> increment, decrement
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
+// reducer
+const counterReducer = (state = 0, action) => {
+  switch(action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+}
+
+// store
+let store = createStore(counterReducer);
+
+
+store.subscribe(() => console.log(store.getState()));
+
+// dispatch
+store.dispatch(increment());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +43,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
