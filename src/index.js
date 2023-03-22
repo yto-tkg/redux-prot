b30b7b611d6dfa85e368fc5f90dc7df9e759418c
yto-tkg <yuto-takagi@cybozu.co.jp>
2023-03-23 08:52:38 +0900

    feat: create store, action

--- src/index.js
+++ src/index.js
@@ -1,45 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {createStore} from "redux"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
+import allReducers from "./reducers";
 
-// actions -> increment, decrement
-const increment = () => {
-  return {
-    type: "INCREMENT",
-  };
-};
+const store = createStore(allReducers);
 
-const decrement = () => {
-  return {
-    type: "DECREMENT",
-  };
-};
-
-// reducer
-const counterReducer = (state = 0, action) => {
-  switch(action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
-}
-
-// store
-let store = createStore(counterReducer);
-
-
-store.subscribe(() => console.log(store.getState()));
-
-// dispatch
-store.dispatch(increment());
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
