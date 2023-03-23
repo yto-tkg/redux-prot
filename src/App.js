e25b5ac99683b49660959a37d0b240819acd1865
yto-tkg <yuto-takagi@cybozu.co.jp>
2023-03-24 08:44:54 +0900

    feat: reducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement, login } from "./actions";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogin = useSelector((state) => state.isLogin);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
+      <h3>カウント{counter}</h3>
+      <button onClick={() => dispatch(increment(7))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(login())}>ログインorログアウト</button>
+      {isLogin ? <h3>ログインに成功</h3> : <h3>ログインしてください</h3>}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import "./index.css";
 import App from "./App";
 import { createStore } from "redux";
 import allReducers from "./reducers";
+import { Provider } from "react-redux";
 
-const store = createStore(allReducers);
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
